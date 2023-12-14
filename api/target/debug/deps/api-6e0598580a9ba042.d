501e16434a96cb2ddffaf08105ec509ead2c2ea3
Nahjay <nahjaybattieste@gmail.com>
2023-12-14 00:22:53 -0500

    feat: created main.py file

---
+++ api/target/debug/deps/api-6e0598580a9ba042.d
@@ -0,0 +1,5 @@
+/home/atomi/Documents/Projects/Raspberry-Pi-Gesture-Controlled-Led-System/api/target/debug/deps/api-6e0598580a9ba042.rmeta: src/main.rs
+
+/home/atomi/Documents/Projects/Raspberry-Pi-Gesture-Controlled-Led-System/api/target/debug/deps/api-6e0598580a9ba042.d: src/main.rs
+
+src/main.rs:
---
+++ api/target/debug/deps/api-af28782c00984f6b.d
@@ -0,0 +1,5 @@
+/home/atomi/Documents/Projects/Raspberry-Pi-Gesture-Controlled-Led-System/api/target/debug/deps/api-af28782c00984f6b.rmeta: src/main.rs
+
+/home/atomi/Documents/Projects/Raspberry-Pi-Gesture-Controlled-Led-System/api/target/debug/deps/api-af28782c00984f6b.d: src/main.rs
+
+src/main.rs:
