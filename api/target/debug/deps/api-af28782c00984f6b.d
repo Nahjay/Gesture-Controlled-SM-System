5bc003ff90c19070f77acad7b3f8454f455591af
Nahjay <nahjaybattieste@gmail.com>
2023-12-14 00:23:32 -0500

    fix: updated gitignore

--- api/target/debug/deps/api-af28782c00984f6b.d
+++
@@ -1,5 +0,0 @@
-/home/atomi/Documents/Projects/Raspberry-Pi-Gesture-Controlled-Led-System/api/target/debug/deps/api-af28782c00984f6b.rmeta: src/main.rs
-
-/home/atomi/Documents/Projects/Raspberry-Pi-Gesture-Controlled-Led-System/api/target/debug/deps/api-af28782c00984f6b.d: src/main.rs
-
-src/main.rs:
