adb63d1c42c6126afd190d1a7001d4d5f259c894
Nahjay <nahjaybattieste@gmail.com>
2023-12-21 01:44:36 -0500

    feat: correct endpoint now works for mobile app

--- mobile-app/GestureApp/App.js
+++ mobile-app/GestureApp/App.js
@@ -3,43 +3,36 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
 export default function App() {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
 
   // Function to start Gesture Recognition
-  function startGestureRecognition() {
-    console.log("Starting Gesture Recognition...")
+  const startGestureRecognition = async () => {
+    console.log("Starting Gesture Recognition...");
 
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
+    try {
+      const response = await fetch('http://192.168.0.161:8080/start_gesture_recognition');
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      setError(error);
+    }
+  };
 
-    const handleClick = async () => {
-      try {
-        const response = await fetch('http://192.168.0.105:8080/start_gesture_recognition');
-        const data = await response.json();
-        setData(data);
-      }
-      catch (error) {
-        setError(error);
-      }
-    };
-
-
-    return (
-      <View style={styles.container}>
-        <Text>Gesture Recognition Project!</Text>
-
-        {/* Add a button to initiate Gesture Recognition */}
-        <Button title='Start Gesture Recognition' onPress={startGestureRecognition} />
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  return (
+    <View style={styles.container}>
+      <Text>Gesture Recognition Project!</Text>
+      <Button title='Start Gesture Recognition' onPress={startGestureRecognition} />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
