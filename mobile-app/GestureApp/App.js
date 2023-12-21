84f1bb31d948b390e9375570d6ef501e821dec55
Nahjay <nahjaybattieste@gmail.com>
2023-12-20 23:40:36 -0500

    wip: added button to serve as a get request to initaite the camera

--- mobile-app/GestureApp/App.js
+++ mobile-app/GestureApp/App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Gesture Recognition Project!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  // Function to start Gesture Recognition
+  function startGestureRecognition() {
+    console.log("Starting Gesture Recognition...")
+
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    const handleClick = async () => {
+      try {
+        const response = await fetch('http://192.168.0.105:5000/start_gesture_recognition');
+        const data = await response.json();
+        setData(data);
+      }
+      catch (error) {
+        setError(error);
+      }
+    };
+
+
+    return (
+      <View style={styles.container}>
+        <Text>Gesture Recognition Project!</Text>
+
+        {/* Add a button to initiate Gesture Recognition */}
+        <Button title='Start Gesture Recognition' onPress={startGestureRecognition} />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+}
